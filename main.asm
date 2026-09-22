7f3d26bb58cc291861eb4e72e7b6b65a67c6ba43
Dominik Daxner <daxndo@ms.spsostrov.cz>
2026-09-22 21:29:14 +0200

    inital commit

---
+++ main.asm
@@ -0,0 +1,28 @@
+; to compile
+; nasm -f bin main.asm -o main.bin 
+; qemu-system-x86_64 main.bin
+[org 0x7c00]
+
+start:
+    mov ebx, string
+    call print_str
+
+    jmp $
+
+; ebx: pointer to the string
+print_str:
+    mov ah, 0x0e
+print_loop:
+    mov al, [ebx]
+    cmp al, 0
+    je print_end
+    int 0x10
+    inc ebx
+    jmp print_loop
+print_end:
+    ret
+
+string: db "Hello, World!", 0
+
+times 510-($-$$) db 0
+dw 0xaa55
